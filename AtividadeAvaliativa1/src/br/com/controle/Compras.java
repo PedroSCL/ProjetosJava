8746c05e15e33ed0c4301542d4e2402c2163702a
Pedro Henrique S.C. Lima <117484349+PedroSCL@users.noreply.github.com>
2024-09-05 10:11:58 -0300

    a

---
+++ AtividadeAvaliativa1/src/br/com/controle/Compras.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author pedrohenrique.clima
+ */
+public class Compras {
+    private double compra1;
+    private double compra2;
+    private double compra3;
+
+    public double getCompra3() {
+        return compra3;
+    }
+
+    public void setCompra3(double compra3) {
+        this.compra3 = compra3;
+    }
+
+    public double getCompra2() {
+        return compra2;
+    }
+
+    public void setCompra2(double compra2) {
+        this.compra2 = compra2;
+    }
+
+    public double getCompra1() {
+        return compra1;
+    }
+
+    public void setCompra1(double compra1) {
+        this.compra1 = compra1;
+        
+    }
+    
+    public Double somar(){
+        return this.compra1 = this.compra2;
+    }
+
+}
