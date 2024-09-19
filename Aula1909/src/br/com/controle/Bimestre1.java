a34172a80a28ec9ae716f2703b291c3d516f5f28
Pedro Henrique S.C. Lima <117484349+PedroSCL@users.noreply.github.com>
2024-09-19 10:41:07 -0300

    NewProjects

---
+++ Aula1909/src/br/com/controle/Bimestre1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author pedrohenrique.clima
+ */
+public class Bimestre1 extends Semestre{
+    public int media(int valor1, int valor2){
+        return ((valor1+valor2)/2);
+    }
+}
